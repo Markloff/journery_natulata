1871302398e842f42b72d3fcef022c4ad37c5c59
marklov <caichenghan1997@gmail.com>
2022-02-22 23:57:56 +0800

    feat: dynamic import

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,6 +1,19 @@
 import React, { useEffect } from 'react';
+
+
 export default function Web() {
 
+	useEffect(() => {
+		console.log('mount');
+
+		const createIpfs = async () => {
+			const { create } = await import("ipfs-core");
+			create().then(res => {
+				console.log(res);
+			})
+		}
+		createIpfs();
+	}, [])
 
 	return (
 		<div>
