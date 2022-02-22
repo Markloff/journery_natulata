d0ef7dfd658bb793d0e302196399d3c56fe4b584
caichenghan <caichenghan@youzan.com>
2022-02-22 23:48:59 +0800

    feat: delete ipfs

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,13 +1,6 @@
 import React, { useEffect } from 'react';
-import { create } from 'ipfs-core';
-
 export default function Web() {
 
-	useEffect(() => {
-		create().then(res => {
-			console.log('res', res);
-		})
-	}, [])
 
 	return (
 		<div>
