5af2ad0b87c20857a5c10716b5b5172084e528b4
Markloff <50124131+Markloff@users.noreply.github.com>
2022-03-17 19:01:01 +0800

    fix: delete code runner dependency

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,7 +2,7 @@ const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
 
 const withPlugins = require('next-compose-plugins');
 
-const withTM = require('next-transpile-modules')(['ui', 'code-runner']);
+const withTM = require('next-transpile-modules')(['ui']);
 
 
 module.exports = withPlugins([
